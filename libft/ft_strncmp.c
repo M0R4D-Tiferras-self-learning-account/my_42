a886107bc5ec926065dc2c848dee0ad4708fff5c
M0R4D-Tiferras-self-learning-account <m.tiferras@gmail.com>
2024-10-26 23:49:53 +0100

    fix header norm

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -1,23 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: moutifer <moutifer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/23 15:37:26 by moutifer          #+#    #+#             */
-/*   Updated: 2024/10/25 20:12:43 by moutifer         ###   ########.fr       */
+/*   Created: 2024/10/26 15:26:46 by moutifer          #+#    #+#             */
+/*   Updated: 2024/10/26 16:59:25 by moutifer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isalnum(int c)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z')
-		|| (c >= 'A' && c <= 'Z'))
+	size_t	idx;
+
+	idx = 0;
+	while ((s1[idx] != '\0') && (s2[idx] != '\0') && (s1[idx] == s2[idx])
+		&& (idx < n))
 	{
-		return (1);
+		idx++;
 	}
-	return (0);
+	return (((unsigned char)(s1[idx])) - ((unsigned char)(s2[idx])));
 }
--- libft/libft.h
+++ libft/libft.h
@@ -11,24 +11,24 @@
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
-void *ft_memset(void *b, int c, size_t len);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
+void	*ft_memset(void *b, int c, size_t len);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-int	ft_atoi(const char *str);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_atoi(const char *str);
 
 #endif
