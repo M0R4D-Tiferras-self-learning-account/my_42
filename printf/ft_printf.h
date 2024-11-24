81a866d017839d0636d2a1860890df2e0ca06cbe
moutifer <moutifer@student.1337.ma>
2024-11-24 21:58:18 +0100

    to_be_continued

--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,14 +6,13 @@
 /*   By: moutifer <moutifer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 19:47:17 by moutifer          #+#    #+#             */
-/*   Updated: 2024/11/24 17:53:45 by moutifer         ###   ########.fr       */
+/*   Updated: 2024/11/24 21:58:12 by moutifer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <libc.h>
 
 int		_strlen(const char *s);
 int		_putchar(char c);
