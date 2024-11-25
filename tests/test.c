f0ba17773ee480d38df2e0191770d99ed0db7b68
moutifer <moutifer@student.1337.ma>
2024-11-25 02:53:00 +0100

    to_be_continued

---
+++ tests/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "../printf/ft_printf.h"
+
+
+// 3284 -> 205R4 -> 12R13 -> 0R12 == 0xCD4
+
+
+int	main(void)
+{
+	// int r = 0, r2 = 0;
+	// r = printf("%s", NULL);
+	// printf("\n");
+	// r2 = ft_printf("%s", NULL);
+	// printf("\nOriginal return; %d\nMine        %d\n", r, r2);
+
+	int n = -123;
+	unsigned int nn;
+	nn = (unsigned int) nn;
+	printf("%u\n", nn);
+	return 0;
+}
