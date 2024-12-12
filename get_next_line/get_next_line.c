43b4189ab1f5809b43a2ef19d6761e228adc69ba
moutifer <moutifer@student.1337.ma>
2024-12-12 04:15:33 +0100

    to_be_continued

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: moutifer <moutifer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 16:23:26 by moutifer          #+#    #+#             */
-/*   Updated: 2024/12/12 04:09:11 by moutifer         ###   ########.fr       */
+/*   Updated: 2024/12/12 04:15:21 by moutifer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,13 @@
 char	*get_next_line(int fd)
 {
 	static int	call;
+	int			read_it;
+	char		*buffer;
+	char		*result;
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE > 2147483647)
+		return (NULL);
+	read_it = read(fd, buffer, BUFFER_SIZE);
+	if (read_it == -1 || read_it == 0)
 		return (NULL);
 }
